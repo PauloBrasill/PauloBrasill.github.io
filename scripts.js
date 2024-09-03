e79e6b7e18f13049d43b201f97f0fe51e2eea148
PauloBrasill <72193669+PauloBrasill@users.noreply.github.com>
2024-09-03 02:12:23 -0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,1 +1,7 @@
-<script src="scripts.js"></script>
+document.getElementById('btn1').addEventListener('click', function() {
+    alert('Botão 1 clicado!');
+});
+
+document.getElementById('btn2').addEventListener('click', function() {
+    alert('Botão 2 clicado!');
+});
